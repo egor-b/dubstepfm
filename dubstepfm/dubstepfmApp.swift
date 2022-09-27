0aa5f480d68768ebb819713dd67d93be70ba1e8a
Egor Bryzgalov <bryzgalov.e@yahoo.com>
2022-09-27 10:34:32 -0400

    Initial Commit

---
+++ dubstepfm/dubstepfmApp.swift
@@ -0,0 +1,17 @@
+//
+//  dubstepfmApp.swift
+//  dubstepfm
+//
+//  Created by Egor Bryzgalov on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct dubstepfmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
